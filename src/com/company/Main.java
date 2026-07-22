7b836856c8bd573e234946b43981ad14c9a9f351
Ibna Masud Rana Parvej <35460533+Masud16@users.noreply.github.com>
2026-07-22 21:22:44 +0600

    code update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,45 +1,22 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-//        Scanner reader = new Scanner(System.in);
-//        Problem11 problem11 = new Problem11();
-//
-//        System.out.print("Enter a year: ");
-//        int yr = reader.nextInt();
-//
-//        String result = problem11.isLeapYear(yr);
-//
-//        System.out.println(result);
-
-
-
-//        Problem1 problem1 = new Problem1();
-//
-//        System.out.print("Enter a Sentence: ");
-//        String abc = reader.next();
-//
-//        String r = problem1.solution1(abc);
-//
-//        System.out.println(r);
-
-
         int x;
         int y;
 
-        x = 589044;
-        y = 189034030;
+        x = 5;
+        y = 10;
 
-        Problem2 problem2 = new Problem2();
+        Sumation s = new Sumation();  // s name ekta object create
 
-        problem2.solution2(x,y);  // method call kortechi
+        int c = s.sumOfTwoNo(x,y);  // s object er maddhome method call kortechi
 
-        int c = problem2.solution2(x,y);
+        int d = s.sumOfThreeNo(2,3,6);
 
-        System.out.println("The sum of " + x + " and " + y + " is: " + c);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
---
+++ src/com/company/Sumation.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Sumation {   //sub class
+//    Java Program to Add Two Integers
+
+    int sumOfTwoNo(int a, int b) { //method
+        return a + b;
+    }
+
+
+    int sumOfThreeNo(int a, int b, int c) { //method
+
+        int z = a + b + c;
+
+        return z;
+    }
+
+}
