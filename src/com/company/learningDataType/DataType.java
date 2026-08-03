bf1859a8740ce8e0398305a76c456793dc788e58
Ibna Masud Rana Parvej <35460533+Masud16@users.noreply.github.com>
2026-08-03 21:17:42 +0600

    Create DataType.java

---
+++ src/com/company/learningDataType/DataType.java
@@ -0,0 +1,39 @@
+package com.company.learningDataType;
+
+public class DataType {
+
+    public static void main(String[] x){
+
+
+        int a;  //null support kore na
+        Integer b; //eta null support kore..Integer er onk gulo method o ache jeta int er nai
+
+
+        long c;
+        Long d;
+
+        String text = "30490";
+
+        b = text.length();
+
+        char ch;
+        String s;
+
+        boolean z = true;
+        Boolean y;
+
+        s = "Represents an immutable sequence of characters enclosed in double quotes";
+
+        System.out.println(s.length());
+
+        if(s.equals("Masud")){
+            System.out.println("OK");
+        }
+        else System.out.println("NO");
+
+
+
+
+
+    }
+}
