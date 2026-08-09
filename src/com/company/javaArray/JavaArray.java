e31a1b628521bff4247b935baf7c0437a61aafe5
Ibna Masud Rana Parvej <35460533+Masud16@users.noreply.github.com>
2026-08-09 20:30:14 +0600

    set , list learned

--- src/com/company/javaArray/JavaArray.java
+++ src/com/company/javaArray/JavaArray.java
@@ -4,30 +4,34 @@ public class JavaArray {
     public static void main(String[] x){
 
 
-        String car = "BMW";
-        String car2 = "Mazda";
-        String car3 = "Fort";
+//        String car = "BMW";
+//        String car2 = "Mazda";
+//        String car3 = "Fort";
+//
+//        String[] cars = {"BMW","Mazda","Fort","XYZ"};
+//
+//        cars[0] = "MND";
+//
+//        System.out.println(cars.length);
+//
+//        for(int i = 0; i < cars.length; i++) {
+//            System.out.println(cars[i]);
+//        }
+//
+//
+//
+//        int a = 5;
+//        int b = 6;
+//        int c = 7;
 
-        String[] cars = {"BMW","Mazda","Fort","XYZ"};
 
-        cars[0] = "MND";
+        int[] nums = {10,20,10,30};
 
-        System.out.println(cars.length);
+        nums[0] = 200;
 
-        for(int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]);
+        for(int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
         }
-
-
-
-        int a = 5;
-        int b = 6;
-        int c = 7;
-
-
-        int[] nums = {5,6,7,8};
-
-        System.out.println(nums[3]);
 
 
 
