8e8d9463a9dc4870a53c163a20b405eabb417fb3
Ibna Masud Rana Parvej <35460533+Masud16@users.noreply.github.com>
2026-09-05 10:37:55 +0600

    Update JavaArray.java

--- src/com/company/javaArray/JavaArray.java
+++ src/com/company/javaArray/JavaArray.java
@@ -4,25 +4,25 @@ public class JavaArray {
     public static void main(String[] x){
 
 
-//        String car = "BMW";
-//        String car2 = "Mazda";
-//        String car3 = "Fort";
-//
-//        String[] cars = {"BMW","Mazda","Fort","XYZ"};
-//
-//        cars[0] = "MND";
-//
-//        System.out.println(cars.length);
-//
-//        for(int i = 0; i < cars.length; i++) {
-//            System.out.println(cars[i]);
-//        }
-//
-//
-//
-//        int a = 5;
-//        int b = 6;
-//        int c = 7;
+        String car = "BMW";
+        String car2 = "Mazda";
+        String car3 = "Fort";
+
+        String[] cars = {"BMW","Mazda","Fort","XYZ"};
+
+        cars[0] = "MND";
+
+        System.out.println(cars.length);
+
+        for(int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
+
+
+        int a = 5;
+        int b = 6;
+        int c = 7;
 
 
         int[] nums = {10,20,10,30};
